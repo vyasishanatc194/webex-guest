98a84f9837472f14f6d4072eaef02ea20fb6d3c3
Ishanvyas <vyasishanatc194@gmail.com>
2021-05-13 10:59:19 +0530

    Update incall page

--- assets/js/participant-new.js
+++ assets/js/participant-new.js
@@ -304,4 +304,99 @@ $(document).ready(function () {
   //   maximize_screen();
   //   maximize_screen_add_class();
   // }
+
+  if (
+    window.matchMedia(
+      "(min-width: 768px) and (max-width: 1024px) and (min-height: 680px)"
+    ).matches
+  ) {
+    // $("#chat-group-participant-area").addClass("mobile-landscape-chat-open");
+
+    $(".chat-btn-item-li").click(function () {
+      $("#chat-group-participant-area").addClass("mobile-landscape-chat-open");
+      $(".incall-area-div").addClass("no-zoom");
+
+      $(".remove-chat-group-btn").click(function () {
+        $("#chat-group-participant-area").removeClass(
+          "mobile-landscape-chat-open"
+        );
+        $(".incall-area-div").removeClass("no-zoom");
+      });
+    });
+
+    /* Default screen */
+    $("#main-small-screen-view-area")
+      .addClass("show-area small-screen-area top-left-side-small-screen")
+      .removeClass("hidden-area");
+    $("#main-small-screen-view-area #small-screen-image").attr(
+      "src",
+      "assets/images/user.jpg"
+    );
+
+    function mobileCloseParticipantScreen() {
+      $("#main-big-screen-view-area .part-list-root-div")
+        .removeClass("show-area")
+        .addClass("hidden-area");
+      $("#goto-presentor-view-area")
+        .removeClass("show-area")
+        .addClass("hidden-area");
+
+      $("#chat-group-participant-area")
+        .removeClass("show-area")
+        .addClass("hidden-area");
+      $("#disabled-chat-btn").removeClass("disabled-btn");
+
+      $("#small-screen-image").removeClass("hidden-area").addClass("show-area");
+      $("#small-screen-video-div")
+        .removeClass("show-area")
+        .addClass("hidden-area");
+
+      $("#in-call-video-bg-fixed-div").removeClass("bg-color-hidden-video");
+      $("#in-call-video-bg-fixed-div .in-call-video-bg").removeClass(
+        "hidden-area"
+      );
+
+      $("#main-small-screen-view-area")
+        .addClass("top-left-side-small-screen")
+        .removeClass("hidden-area");
+
+      // $("#new-tplf-screen-div")
+      //   .removeClass("hidden-area")
+      //   .addClass("show-area");
+
+      $("#more-sm-check02-03").prop("checked", false);
+    }
+
+    $(document).on("change", "#more-sm-check02-03", function () {
+      if ($(this).is(":checked")) {
+        // console.log("if");
+        $(this).closest(".dropdown-menu").prev().dropdown("toggle");
+
+        maximize_screen();
+        $("#main-small-screen-view-area")
+          .addClass("show-area small-screen-area")
+          .removeClass("hidden-area");
+        $("#show-participants-btn-area")
+          .addClass("hidden-area")
+          .removeClass("show-area");
+
+        $("#main-small-screen-view-area").removeClass(
+          "small-screen-area top-left-side-small-screen"
+        );
+
+        // $("#new-tplf-screen-div").addClass("hidden-area").removeClass("show-area");
+
+        $("#goto-presentor-view-btn").click(function () {
+          mobileCloseParticipantScreen();
+        });
+      } else {
+        // console.log("else");
+        $(this).closest(".dropdown-menu").prev().dropdown("toggle");
+
+        if ($("#more-sm-check02-03").prop("checked", false)) {
+          mobileCloseParticipantScreen();
+        }
+      }
+    });
+  }
 });
